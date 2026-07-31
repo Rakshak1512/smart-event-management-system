f983032ef8256d6e4d74059d9305f70ec5c3600f
Rakshak <rakshak@users.noreply.github.com>
2026-07-31 21:47:03 +0530

    Update frontend API URL

--- axios.js
+++ axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+const baseURL = import.meta.env.VITE_API_URL || 'https://smart-event-management-system-1-f2en.onrender.com'
 
 const api = axios.create({
   baseURL,
